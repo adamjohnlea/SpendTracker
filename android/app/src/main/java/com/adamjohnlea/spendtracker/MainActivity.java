0c81d7f56ee161ef6d0d550cbf6f84867697214b
adamjohnlea <imnogeekdptcom@me.com>
2020-05-09 12:16:06 -0600

    first commit

---
+++ android/app/src/main/java/com/adamjohnlea/spendtracker/MainActivity.java
@@ -0,0 +1,6 @@
+package com.adamjohnlea.spendtracker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/application.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+
+class Application extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Spend Tracker",
+      theme: ThemeData(
+        primarySwatch: Colors.green
+      ),
+      home: HomePage()
+    );
+  }
+}
